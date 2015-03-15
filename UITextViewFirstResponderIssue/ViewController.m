c8efba3206661c08282e60c2df29ff86e1726b83
Artyom Kiselev <artyomkiss@me.com>
2015-03-15 21:42:58 +0300

    initial

---
+++ UITextViewFirstResponderIssue/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UITextViewFirstResponderIssue
+//
+//  Created by Artem Kiselev on 15/03/15.
+//  Copyright (c) 2015 Artyom Kiselev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
